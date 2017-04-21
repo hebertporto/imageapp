0f05ed7a75ff070cf555d34fa4cdd27ae8b39693
Hebert Porto <hebertporto@gmail.com>
2017-04-21 17:32:51 -0300

    configurado o plugin de react-camera

--- src/modules/main/components/MainScreen.js
+++ src/modules/main/components/MainScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from './../actions/index';
+
+import Camera from 'react-native-camera';
 
 
 class MainScreen extends Component {
@@ -13,29 +15,75 @@ class MainScreen extends Component {
       gestures: null,
     }
   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Criado no State'
+    };
+    this.takePicture = this.takePicture.bind(this);
+  }
 
   async componentWillMount() {
     const { mainActions } = this.props;
     await mainActions.sayHello();
-    console.log('construcor ! ');
+    await this.setState({
+      msg: this.props.mainState.msg.hello
+    });
   }
 
   render() {
     return (
-      <View>
-        <Text>
-          Main Screen
-        </Text>
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+        >
+          <Text
+            style={styles.capture}
+            onPress={this.takePicture}
+          > [CAPTURE] aaa </Text>
+        </Camera>
       </View>
     );
   }
+
+  takePicture() {
+    const options = {};
+    this.camera.capture({ metadata: options })
+      .then(data => console.log('data foto', data))
+      .catch(err => console.error('error foto', err));
+  }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
 
 export default connect(
   state => ({
-    mainState: state.main
+    mainState: state.main // dá acesso ao que é atualizado pelos REDUCERS
   }),
   dispatch => ({
-    mainActions: bindActionCreators(actions, dispatch)
+    mainActions: bindActionCreators(actions, dispatch) // dá acesso as funções no arquivo INDEX
   })
 )(MainScreen);
