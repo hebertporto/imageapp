37c0375f9f91a7685bd133f13e9a0422356da4b6
Hebert Porto <hebertporto@gmail.com>
2017-05-17 22:37:40 -0300

    colocando listagem de aulas cadastradas em nova aula

--- src/modules/createClass/components/CreateClassScreen.js
+++ src/modules/createClass/components/CreateClassScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ListView } from 'react-native';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -21,9 +21,16 @@ class CreateClassScreen extends Component {
       horaInicio: '',
       horaFim: '',
       diaSemana: '',
-      nomeAula: ''
+      nomeAula: '',
+      list: [{ name: 'Osmar' }, { name: 'Xaxaxa' }, { name: 'Kakakaka' }],
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      })
     };
     this.onSubmit = this.onSubmit.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+    this.renderHeader = this.renderHeader.bind(this);
+
   }
 
   async componentWillMount() {
@@ -45,7 +52,11 @@ class CreateClassScreen extends Component {
     }
   }
 
-  render() {
+  renderRow(item) {
+    return <Text>{item.name} </Text>;
+  }
+
+  renderHeader() {
     const { horaInicio, horaFim, diaSemana, nomeAula } = this.state;
     return (
       <View>
@@ -90,11 +101,26 @@ class CreateClassScreen extends Component {
             underlineColorAndroid="transparent"
           />
         </View>
+      </View>
+    );
+  }
+
+  render() {
+    const ds = this.state.dataSource;
+    const list = this.state.list;
+    return (
+      <View>
         <TouchableOpacity style={styles.loginButtonContainer} onPress={this.onSubmit}>
           <View style={styles.loginButton}>
             <Text style={styles.loginText}>Cadastrar Aula</Text>
           </View>
         </TouchableOpacity>
+        <ListView
+          enableEmptySections
+          dataSource={ds.cloneWithRows(list)}
+          renderRow={this.renderRow}
+          renderHeader={this.renderHeader}
+        />
       </View>
     );
   }
