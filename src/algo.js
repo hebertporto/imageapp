01c204aeeab6af0476e079b4cbb36b22e964d43c
Hebert Porto <hebertporto@gmail.com>
2017-05-31 13:09:31 -0300

    ajustes

--- src/algo.js
+++ src/algo.js
@@ -8,56 +8,6 @@ const horaInit = hour + min;
 // console.log('data', horaInit);
 // console.log('dia da semana', day);
 
-const curso = {
-  1: [
-    {
-      horario: '1600-1930',
-      curso: 'Sistemas'
-    },
-    {
-      horario: '2000-2230',
-      curso: 'Banco de Dados'
-    }
-  ],
-  2: [
-    {
-      horario: '2000-2230',
-      curso: 'Sistemas2'
-    },
-  ]
-};
-
-const galley = [
-  1: [
-    {
-      class: 'Web',
-      pictures: [
-        {
-          date: '8/05/2017',
-          url:'Image 1'
-        },
-        {
-          date: '8/05/2017',
-          url:'Image 2'
-        },
-        {
-          date: '23/05/2017',
-          url:'Image 3'
-        }
-      ]
-    },
-    {
-      class: 'Banco de Dados',
-      pictures: [
-        {
-          date: '15/05/2017',
-          url:'Image 1'
-        }
-      ]
-    }
-  ]
-];
-
 
 const diaDoCurso = 1;
 const novoCurso = {
@@ -102,4 +52,3 @@ const res = curso[dayWeek].filter((value) => {
   }
 });
 console.log(res[0].curso);
-
